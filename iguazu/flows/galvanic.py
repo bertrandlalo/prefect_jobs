2802a63c260414262fa89b6e39d5ccc73a187d21
David Ojeda <david.ojeda@gmail.com>
2019-06-17 19:30:54 +0200

    Add class versions of Raph's task-decorated pipeline

--- iguazu/flows/galvanic.py
+++ iguazu/flows/galvanic.py
@@ -1,98 +1,142 @@
+import os
+import time
+
 from prefect.utilities.debug import raise_on_exception
-from time import time
 from prefect.engine.executors import DaskExecutor, LocalExecutor, SynchronousExecutor
 from prefect import Flow, Parameter, context
-logger = context.get("logger")
-from prefect import unmapped
-
-
-
-galvanic_clean_params = Parameter("galvanic_clean_params",
-                                  default={"warmup_duration": 30, "column_name": "F",
-                                            "corrupted_maxratio": .3,
-                                            "glitch_params": {"scaling": "robust",
-                                             "nu": 1, "range": [-0.02, +0.02],  "rejection_win": 20},
-                                            "interpolation_params": {"method": "cubic"},
-                                            "lowpass_params": {"Wn": [35], "order": 5},
-                                            "scaling_params":  {"method": "standard"}})
-
-galvanic_cvx_params = Parameter("galvanic_cvx_params", {"warmup_duration":  15,
-                                    "column_name": "F_clean_inversed_lowpassed_zscored",
-                                    "glitch_params":  {"scaling": False,
-                                                     "nu": 0,
-                                                     "range": [0, 4],
-                                                     "rejection_win": 20},
-                                                        "cvxeda_params": None})
-
-galvanic_scrpeaks_params = Parameter("galvanic_scrpeaks_params", {"warmup_duration": 15, "column_name": "F_clean_inversed_lowpassed_zscored_SCR",
-                                                        "peaks_params": {"width": .5,
-                                                                        "prominence": .1,
-                                                                        "prominence_window": 15},
-                                                        "glitch_params":  {"nu": 0, "range": [0, 7]}})
-
-
 import click
+
+from iguazu.tasks.common import list_files, convert_to_file_proxy
+from iguazu.tasks.galvanic import CleanSignal, ApplyCVX, DetectSCRPeaks
+from iguazu.tasks.quetzal import Query, ConvertToFileProxy
+
+
 @click.command()
-@click.option('-b', '--basedir', type=click.Path(file_okay=False, dir_okay=True, exists=True),
+@click.option('-b', '--base-dir', type=click.Path(file_okay=False, dir_okay=True, exists=True),
               required=True, help='Path from where the files with raw data are read. ')
-@click.option('-o', '--outputdir', type=click.Path(file_okay=False, dir_okay=True, exists=False),
+@click.option('-o', '--output-dir', type=click.Path(file_okay=False, dir_okay=True, exists=False),
               required=True, help='Path where the files with processed data are saved. ')
-@click.option('--executor_type', type=click.Choice(['local', 'synchronous', 'dask']),
-              help='Type of executor to run the flow. ')
-@click.option('--visualize_flow', is_flag=True,
+@click.option('--executor-type', type=click.Choice(['local', 'synchronous', 'dask']),
+              default='local', help='Type of executor to run the flow. Default is local.')
+@click.option('--visualize-flow', is_flag=True,
               help='Whether to visualize the flow graphs')
 @click.option('--force', is_flag=True,
               help='Whether to force the processing if the path already exists in the output file. ')
-
-def cli(basedir, outputdir, executor_type, visualize_flow, force):
+def cli(base_dir, output_dir, executor_type, visualize_flow, force):
 
     """ Run the HDF5 pipeline on the specified FILENAMES.
     Do not specify any FILENAMES to run on *all* files found on DATAFOLDER.
     """
-    from iguazu.tasks.galvanic import etl_galvanic_clean, etl_galvanic_cvx, etl_galvanic_scrpeaks
-    from iguazu.tasks.common import list_files, io_filenames
-    with Flow("process gsr") as flow:
-        basedir_param = Parameter("basedir")
-        outputdir_param = Parameter("outputdir")
-
-        force_clean = Parameter("force_clean")
-        force_cvx = Parameter("force_cvx")
-        force_scrpeaks = Parameter("force_scrpeaks")
-
-        list_files = list_files(basedir_param)
-        io_filenames1 = io_filenames.map(list_files, unmapped(outputdir_param))
-        etl_galvanic_clean = etl_galvanic_clean.map(io_filenames=io_filenames1,
-                                                    tranform_params=unmapped(galvanic_clean_params),
-                                                    force=unmapped(force_clean))
-        io_filenames2 = io_filenames.map(etl_galvanic_clean)
-        etl_galvanic_cvx = etl_galvanic_cvx.map(io_filenames=io_filenames2,
-                                                tranform_params=unmapped(galvanic_cvx_params),
-                                                force=unmapped(force_cvx))
-        io_filenames3 = io_filenames.map(etl_galvanic_cvx)
-        etl_galvanic_scrpeaks = etl_galvanic_scrpeaks.map(io_filenames=io_filenames3,
-                                                          tranform_params=unmapped(galvanic_scrpeaks_params),
-                                                          force=unmapped(force_scrpeaks))
-    if visualize_flow:
-        flow.visualize()
-    t0 = time()
     if executor_type == 'dask':
-        executor = DaskExecutor(local_processes=True, memory_limit=30 * 2 ** 30)
-    elif executor_type=="synchronous":
+        executor = DaskExecutor(local_processes=True)#, memory_limit=30 * 2 ** 30)
+        # executor = DaskExecutor('localhost:8786')
+    elif executor_type == "synchronous":
         executor = SynchronousExecutor()
     else: # default
         executor = LocalExecutor()
 
-    with raise_on_exception():
-        flow_state = flow.run(executor=executor,
-                              parameters={"basedir": basedir,
-                                          "outputdir": outputdir,
-                                          "force_clean": force, "force_cvx": force, "force_scrpeaks": force})
-    local_execution_duration = time() - t0
-    print("{executor_type} executor ran in {duration} seconds".format(executor_type=executor_type,
-                                                                      duration=local_execution_duration))
+    # Context/global arguments
+    context_args = dict(
+        quetzal_client=dict(
+            url=os.getenv('QUETZAL_URL', 'https://quetzal.omind.me/api/v1'),
+            username=os.getenv('QUETZAL_USER', ''),
+            password=os.getenv('QUETZAL_PASSWORD', ''),
+            insecure=True,
+        ),
+        temp_dir=output_dir,
+    )
+
+    # Tasks and task arguments
+    quetzal_query = Query(name='Query quetzal')
+    convert_query = ConvertToFileProxy(id_key='id')
+    clean_signal = CleanSignal(
+        warmup_duration=30,
+        glitch_kwargs=dict(
+            scaling='robust',
+            nu=1,
+            range=(-0.02, +0.02),
+            rejection_win=20,
+        ),
+        interpolation_kwargs=dict(
+            method='cubic',
+        ),
+        lowpass_kwargs=dict(
+            Wn=[35],
+            order=5,
+        ),
+        scaling_kwargs=dict(
+            method='standard',
+        ),
+        corrupted_maxratio=0.3,
+        force=force,
+    )
+    apply_cvx = ApplyCVX(
+        warmup_duration=15,
+        glitch_kwargs=dict(
+            scaling=False,
+            nu=0,
+            range=(0, 4),
+            rejection_win=20,
+        ),
+        cvxeda_kwargs=None,
+        force=force,
+    )
+    detect_scr_peaks = DetectSCRPeaks(
+        warmup_duration=15,
+        glitch_kwargs=dict(
+            nu=0,
+            range=(0, 7),
+        ),
+        peak_detection_kwargs=dict(
+            width=0.5,
+            prominence=0.1,
+            prominence_window=15,
+        ),
+        force=force,
+    )
+
+    # Flow/runtime arguments
+    mode = 'local'  # TODO: move to click
+    flow_parameters = dict()
+    if mode == 'quetzal':
+        flow_parameters['query'] = """
+        SELECT * FROM base
+        WHERE filename LIKE '%.hdf5'
+        LIMIT 10
+        """
+    elif mode == 'local':
+        flow_parameters['query'] = base_dir
+    else:
+        raise ValueError(f'Unknown mode "{mode}"')
+
+    # Flow definition
+    with Flow('test') as flow:
+
+        query = Parameter('query')
+
+        if mode == 'quetzal':
+            rows = quetzal_query(query)
+            raw_signals = convert_query(rows, workspace_id=None)
+        else:
+            rows = list_files(query)
+            raw_signals = convert_to_file_proxy(rows)
+
+        clean_signals = clean_signal.map(signal=raw_signals,
+                                         events=raw_signals)
+        cvx = apply_cvx.map(clean_signals)
+        scr = detect_scr_peaks.map(cvx)
+
+    # Flow execution
+    t0 = time.time()
+    with raise_on_exception(), context(**context_args):
+        flow_state = flow.run(parameters=flow_parameters,
+                              executor=executor)
+    local_execution_duration = time.time() - t0
+    print(f'{executor_type} executor ran in {local_execution_duration} seconds')
 
     if visualize_flow:
         flow.visualize(flow_state=flow_state)
+
 
 
 if __name__ == '__main__':    # __name__ is the process id, that decides for what the process is supposed to work on
--- iguazu/tasks/galvanic.py
+++ iguazu/tasks/galvanic.py
@@ -1,15 +1,20 @@
+from typing import Dict, Optional
 import os
 
 from prefect import task
 from prefect.engine import signals
+import prefect
 import pandas as pd
 
 from iguazu.functions.galvanic import galvanic_cvx, galvanic_scrpeaks, galvanic_clean
 from iguazu.functions.common import path_exists_in_hdf5, safe_read_hdf5
+from iguazu.helpers.files import FileProxy
+
 
 @task
 def etl_galvanic_clean(io_filenames,  tranform_params, force=False,
-                       input_paths = {"data": "/nexus/signal/nexus_signal_raw", "events": "/unity/events/unity_events"}, output_path = "/gsr/timeseries/preprocessed"):
+                       input_paths = {"data": "/nexus/signal/nexus_signal_raw", "events": "/unity/events/unity_events"},
+                       output_path = "/gsr/timeseries/preprocessed"):
     """
     This task is a basic ETL where the input and output are HDF5 files and where the transformation is made on a DataFrame.
     It consists in loading the data from an input file, applying some processing (transformations) and
@@ -53,6 +58,127 @@ def etl_galvanic_clean(io_filenames,  tranform_params, force=False,
     if data.empty:
         raise signals.SKIP(message="cleaning failed") #TODO: I'm not sure how to handle failures
     return str(output_filename)
+
+
+class CleanSignal(prefect.Task):
+
+    def __init__(self,
+                 signal_group: Optional[str] = None,
+                 events_group: Optional[str] = None,
+                 output_group: Optional[str] = None,
+                 signal_column: Optional[str] = None,
+                 warmup_duration: int = 30,
+                 glitch_kwargs: Optional[Dict] = None,
+                 interpolation_kwargs: Optional[Dict] = None,
+                 lowpass_kwargs: Optional[Dict] = None,
+                 scaling_kwargs: Optional[Dict] = None,
+                 corrupted_maxratio: Optional[float] = None,
+                 force: bool = False,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.signal_group = signal_group
+        self.events_group = events_group
+        self.output_group = output_group
+        self.signals_column = signal_column
+        self.warmup_duration = warmup_duration
+        self.glitch_kwargs = glitch_kwargs or {}
+        self.interpolation_kwargs = interpolation_kwargs or {}
+        self.lowpass_kwargs = lowpass_kwargs or {}
+        self.scaling_kwargs = scaling_kwargs or {}
+        self.corrupted_maxratio = corrupted_maxratio or 100
+        self.force = force
+
+    def run(self,
+            signal: FileProxy,
+            events: FileProxy) -> FileProxy:
+
+        output = signal.make_child(suffix='_clean')
+        self.logger.info('Galvanic preprocessing for signal=%s, events=%s -> %s',
+                         signal, events, output)
+
+        # Notes on parameter management
+        #
+        # if I wanted to admit the rewrite of a parameter foo,
+        # 1. Add foo to run parameter as an optional parameter with default None
+        # 2.a Manage None with `foo = foo or self.foo`
+        #
+        # If I wanted to admit a global context value of parameter foo
+        # 2.b `foo = foo or self.foo or context.get('foo', None)`
+        #
+        # Finally, if a default value is needed
+        # 2.c `foo = foo or self.foo or context.get('foo', 'default_value')`
+        #
+        # In the following lines, we are not following these ideas yet. Maybe later.
+        signal_group = self.signal_group or '/nexus/signal/nexus_signal_raw'
+        events_group = self.events_group or '/unity/events/unity_events'
+        signals_column = self.signals_column or 'F'
+        output_group = self.output_group or '/gsr/timeseries/preprocessed'
+
+        # Our current force detection code
+        if not self.force and path_exists_in_hdf5(output.file, output_group):
+            # TODO: consider a function that uses a FileProxy, in particular a
+            #       QuetzalFile. In this case, we could read the metadata
+            #       instead of downloading the file!
+            self.logger.info('Output already exists, skipping')
+            # raise signals.SKIP('Output already exists', result=output) #  Does not work!
+            # TODO: consider a way to raise a skip with results. Currently, the
+            #       only way I think this is possible is by making a new signal
+            #       that derives from PrefectStateSignal and that uses a new
+            #       custom state class as well.
+            return output
+
+        signal_file = signal.file.resolve()
+        events_file = events.file.resolve()
+
+        with pd.HDFStore(signal_file, 'r') as signal_store, \
+             pd.HDFStore(events_file, 'r') as events_store:
+
+            try:
+                # TODO discuss: select column before sending it to a column
+                df_signals = pd.read_hdf(signal_store, signal_group)
+                df_events = pd.read_hdf(events_store, events_group)
+
+                clean = galvanic_clean(df_signals, df_events, signals_column,
+                                       self.warmup_duration,
+                                       self.glitch_kwargs,
+                                       self.interpolation_kwargs,
+                                       self.lowpass_kwargs,
+                                       self.scaling_kwargs,
+                                       self.corrupted_maxratio)
+                meta = {
+                    'state': 'SUCCESS',
+                    'version': '0.0',
+                    'bad_ratio': clean.bad.mean(),
+                }
+            except Exception as ex:
+                self.logger.warning('Galvanic clean graceful fail: %s', ex)
+                clean = pd.DataFrame()
+                meta = {
+                    'state': 'FAILURE',
+                    'version': '0.0',
+                    'exception': str(ex),
+                }
+
+        # TODO: re-code the failure handling with respect to a task parameter
+        # if fail_mode == 'grace': ==> generate empty dataframe, set metadata, return file (prefect raises success)
+        # if fail_mode == 'skip':  ==> generate empty dataframe, set metadata, raise skip
+        # if fail_mode == 'fail':  ==> raise exception as it arrives
+
+        # Manage output, save to file
+        output_file = output.file
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        with pd.HDFStore(output_file, 'w') as output_store:
+            clean.to_hdf(output_store, output_group)
+            output_store.get_node(output_group)._v_attrs['meta'] = {
+                'galvanic': meta,
+            }
+
+        # Set meta on FileProxy so that Quetzal knows about this metadata
+        output.metadata['galvanic'].update(meta)
+        output.upload()
+        output._local_path = None
+
+        return output
 
 
 @task
@@ -101,6 +227,90 @@ def etl_galvanic_cvx(io_filenames,  tranform_params, force=False, input_paths = 
     return str(output_filename)
 
 
+class ApplyCVX(prefect.Task):
+    def __init__(self,
+                 signal_group: Optional[str] = None,
+                 output_group: Optional[str] = None,
+                 signal_column: Optional[str] = None,
+                 warmup_duration: int = 15,
+                 glitch_kwargs: Optional[Dict] = None,
+                 cvxeda_kwargs: Optional[Dict] = None,
+                 force: bool = False,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.signal_group = signal_group
+        self.output_group = output_group
+        self.signals_column = signal_column
+        self.warmup_duration = warmup_duration
+        self.glitch_kwargs = glitch_kwargs or {}
+        self.cvxeda_kwargs = cvxeda_kwargs or {}
+        self.force = force
+
+    def run(self, signal: FileProxy) -> FileProxy:
+        output = signal.make_child(suffix='_cvx')
+        self.logger.info('Galvanic CVXEDA for %s -> %s', signal, output)
+
+        signal_group = self.signal_group or '/gsr/timeseries/preprocessed'
+        signals_column = self.signals_column or 'F_clean_inversed_lowpassed_zscored'
+        output_group = self.output_group or '/gsr/timeseries/deconvoluted'
+
+        # Our current force detection code
+        if not self.force and path_exists_in_hdf5(output.file, output_group):
+            # TODO: consider a function that uses a FileProxy, in particular a
+            #       QuetzalFile. In this case, we could read the metadata
+            #       instead of downloading the file!
+            self.logger.info('Output already exists, skipping')
+            # raise signals.SKIP('Output already exists', result=output) #  Does not work!
+            # TODO: consider a way to raise a skip with results. Currently, the
+            #       only way I think this is possible is by making a new signal
+            #       that derives from PrefectStateSignal and that uses a new
+            #       custom state class as well.
+            return output
+
+        signal_file = signal.file
+
+        with pd.HDFStore(signal_file, 'r') as signal_store:
+
+            try:
+                # TODO discuss: select column before sending it to a column
+                df_signals = pd.read_hdf(signal_store, signal_group)
+                cvx = galvanic_cvx(df_signals,
+                                   signals_column,
+                                   self.warmup_duration,
+                                   self.glitch_kwargs,
+                                   self.cvxeda_kwargs)
+                meta = {
+                    'state': 'SUCCESS',
+                    'version': '0.0',
+                    'bad_ratio': cvx.bad.mean(),
+                }
+            except Exception as ex:
+                self.logger.warning('Galvanic CVX graceful fail: %s', ex)
+                cvx = pd.DataFrame()
+                meta = {
+                    'state': 'FAILURE',
+                    'version': '0.0',
+                    'exception': str(ex),
+                }
+
+        # TODO: re-code the failure handling with respect to a task parameter
+        # if fail_mode == 'grace': ==> generate empty dataframe, set metadata, return file (prefect raises success)
+        # if fail_mode == 'skip':  ==> generate empty dataframe, set metadata, raise skip
+        # if fail_mode == 'fail':  ==> raise exception as it arrives
+
+        # Manage output, save to file
+        output_file = output.file
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        with pd.HDFStore(output_file, 'w') as output_store:
+            cvx.to_hdf(output_store, output_group)
+            output_store.get_node(output_group)._v_attrs['meta'] = {
+                'galvanic': meta,
+            }
+
+        # Set meta on FileProxy so that Quetzal knows about this metadata
+        output.metadata['galvanic'].update(meta)
+
+        return output
 
 
 @task
@@ -149,3 +359,90 @@ def etl_galvanic_scrpeaks(io_filenames,  tranform_params, force=False,
     if data.empty:
         raise signals.SKIP(message="scrpeaks failed") #TODO: I'm not sure how to handle failures
     return str(output_filename)
+
+
+class DetectSCRPeaks(prefect.Task):
+
+    def __init__(self,
+                 signal_group: Optional[str] = None,
+                 output_group: Optional[str] = None,
+                 signal_column: Optional[str] = None,
+                 warmup_duration: int = 15,
+                 glitch_kwargs: Optional[Dict] = None,
+                 peak_detection_kwargs: Optional[Dict] = None,
+                 force: bool = False,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.signal_group = signal_group
+        self.output_group = output_group
+        self.signals_column = signal_column
+        self.warmup_duration = warmup_duration
+        self.glitch_kwargs = glitch_kwargs or {}
+        self.peak_detection_kwargs = peak_detection_kwargs or {}
+        self.force = force
+
+    def run(self, signal: FileProxy) -> FileProxy:
+        output = signal.make_child(suffix='_scr')
+        self.logger.info('Galvanic SCR peak detection for %s -> %s', signal, output)
+
+        signal_group = self.signal_group or '/gsr/timeseries/deconvoluted'
+        signals_column = self.signals_column or 'F_clean_inversed_lowpassed_zscored_SCR'
+        output_group = self.output_group or '/gsr/timeseries/scrpeaks'
+
+        # Our current force detection code
+        if not self.force and path_exists_in_hdf5(output.file, output_group):
+            # TODO: consider a function that uses a FileProxy, in particular a
+            #       QuetzalFile. In this case, we could read the metadata
+            #       instead of downloading the file!
+            self.logger.info('Output already exists, skipping')
+            # raise signals.SKIP('Output already exists', result=output) #  Does not work!
+            # TODO: consider a way to raise a skip with results. Currently, the
+            #       only way I think this is possible is by making a new signal
+            #       that derives from PrefectStateSignal and that uses a new
+            #       custom state class as well.
+            return output
+
+        signal_file = signal.file
+
+        with pd.HDFStore(signal_file, 'r') as signal_store:
+
+            try:
+                # TODO discuss: select column before sending it to a column
+                df_signals = pd.read_hdf(signal_store, signal_group)
+                scr = galvanic_scrpeaks(df_signals,
+                                        signals_column,
+                                        self.warmup_duration,
+                                        self.peak_detection_kwargs,
+                                        self.glitch_kwargs)
+                meta = {
+                    'state': 'SUCCESS',
+                    'version': '0.0',
+                    'bad_ratio': scr.bad.mean(),
+                }
+            except Exception as ex:
+                self.logger.warning('Galvanic SCR peak detection graceful fail: %s', ex)
+                scr = pd.DataFrame()
+                meta = {
+                    'state': 'FAILURE',
+                    'version': '0.0',
+                    'exception': str(ex),
+                }
+
+        # TODO: re-code the failure handling with respect to a task parameter
+        # if fail_mode == 'grace': ==> generate empty dataframe, set metadata, return file (prefect raises success)
+        # if fail_mode == 'skip':  ==> generate empty dataframe, set metadata, raise skip
+        # if fail_mode == 'fail':  ==> raise exception as it arrives
+
+        # Manage output, save to file
+        output_file = output.file
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        with pd.HDFStore(output_file, 'w') as output_store:
+            scr.to_hdf(output_store, output_group)
+            output_store.get_node(output_group)._v_attrs['meta'] = {
+                'galvanic': meta,
+            }
+
+        # Set meta on FileProxy so that Quetzal knows about this metadata
+        output.metadata['galvanic'].update(meta)
+
+        return output
