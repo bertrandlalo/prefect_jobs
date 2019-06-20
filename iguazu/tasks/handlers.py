e09e328a166a6f9bd450c501477436ced1aa6e52
David Ojeda <david.ojeda@gmail.com>
2019-06-20 18:21:54 +0200

    Use slug on log name for unique names

--- iguazu/tasks/handlers.py
+++ iguazu/tasks/handlers.py
@@ -14,14 +14,14 @@ class CustomFileHandler(logging.FileHandler):
 def logging_handler(task, old_state, new_state):
     if new_state.is_running():
         if 'temp_dir' not in context or not context.temp_dir:
-            logger.warning('Cannot manage logs without a temp dir')
+            logger.info('Logs cannot be saved without a temp_dir')
             return new_state
 
         log_filename = (
             pathlib.Path(context.temp_dir) /
             'logs' /
             context.scheduled_start_time.strftime('%Y%m%d-%H%M%S') /
-            context.task_full_name
+            f'{context.task_full_name}-{task.slug}'
         ).with_suffix('.log')
         log_filename.parent.mkdir(parents=True, exist_ok=True)
         handler = CustomFileHandler(str(log_filename.resolve()), mode='a')
@@ -40,7 +40,7 @@ def logging_handler(task, old_state, new_state):
                     'logs' /
                     context.scheduled_start_time.strftime('%Y%m%d-%H%M%S') /
                     str(type(new_state).__name__).upper() /
-                    context.task_full_name
+                    f'{context.task_full_name}-{task.slug}'
                 ).with_suffix('.log')
                 final_filename.parent.mkdir(parents=True, exist_ok=True)
                 shutil.move(hdlr.baseFilename, final_filename)
