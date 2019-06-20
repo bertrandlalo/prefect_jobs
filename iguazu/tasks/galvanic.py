71b04ac28139da6bbcc40a2bb56f8b33de2919df
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-06-20 13:14:11 +0200

    Remove decorated tasks that have been rewriten in class

--- iguazu/tasks/galvanic.py
+++ iguazu/tasks/galvanic.py
@@ -11,54 +11,21 @@ from iguazu.functions.common import path_exists_in_hdf5, safe_read_hdf5
 from iguazu.helpers.files import FileProxy
 
 
-@task
-def etl_galvanic_clean(io_filenames,  tranform_params, force=False,
-                       input_paths = {"data": "/nexus/signal/nexus_signal_raw", "events": "/unity/events/unity_events"},
-                       output_path = "/gsr/timeseries/preprocessed"):
-    """
-    This task is a basic ETL where the input and output are HDF5 files and where the transformation is made on a DataFrame.
-    It consists in loading the data from an input file, applying some processing (transformations) and
-    saving it into a file (same or other).
-
-    Parameters
-    ----------
-    io_filenames: dictionary with key 'input' and 'output', containing the input and output file names
-    tranform_params: dictionary with parameters to map when calling the transformation function
-    force: whether or not to force the task if the output path already exists in the output DataFrame
-    input_paths: list of paths that are expected to be found in the input DataFrame
-    output_path: path where the result is stored in the output DataFrame
-
-    Returns
-    -------
-    output filename with new path /gsr/timeseries/preprocessed
-    """
-
-    input_filename = io_filenames["in"]
-    output_filename = io_filenames["out"]
-
-    if not force and path_exists_in_hdf5(output_filename, output_path):
-        return str(output_filename)
-
-    data_params = {key: safe_read_hdf5(input_filename, input_paths[key]) for key in input_paths}
-
-    print("Cleaning of gsr from file {stem}.".format(stem=os.path.basename(input_filename)))
-    #Define parameter for 'clean_galvanic'
-
-    try:
-        data = galvanic_clean(**data_params, **tranform_params)
-        meta = {"state": "success", "version": 0.0, "exception": None, "bad_ratio": data.bad.mean() }
-    except Exception as e:
-        data = pd.DataFrame()
-        meta = {"state": "fail", "version": 0.0, "exception": str(e), "bad_ratio": None}
-
-    # save in hdf
-    with pd.HDFStore(output_filename, "a") as store:
-        data.to_hdf(store, output_path)
-        store.get_node(output_path)._v_attrs['meta'] = meta
-    if data.empty:
-        raise signals.SKIP(message="cleaning failed") #TODO: I'm not sure how to handle failures
-    return str(output_filename)
-
+#     This task is a basic ETL where the input and output are HDF5 files and where the transformation is made on a DataFrame.
+#     It consists in loading the data from an input file, applying some processing (transformations) and
+#     saving it into a file (same or other).
+#
+#     Parameters
+#     ----------
+#     io_filenames: dictionary with key 'input' and 'output', containing the input and output file names
+#     tranform_params: dictionary with parameters to map when calling the transformation function
+#     force: whether or not to force the task if the output path already exists in the output DataFrame
+#     input_paths: list of paths that are expected to be found in the input DataFrame
+#     output_path: path where the result is stored in the output DataFrame
+#
+#     Returns
+#     -------
+#     output filename with new path /gsr/timeseries/preprocessed
 
 class CleanSignal(prefect.Task):
 
@@ -75,6 +42,27 @@ class CleanSignal(prefect.Task):
                  corrupted_maxratio: Optional[float] = None,
                  force: bool = False,
                  **kwargs):
+        '''
+
+        Parameters
+        ----------
+        signal_group: group in the input hdf5 where the input signals are stored.
+        Default to '/nexus/signal/nexus_signal_raw'.
+        events_group: group in the input  hdf5 where the input events are stored.
+        Default to '/unity/events/unity_events'.
+        output_group: group in the output  hdf5 to store the output data. Default to
+        '/gsr/timeseries/preprocessed'
+        signal_column: column in the input signal to select. Default to 'F'.
+        warmup_duration: see the documentation of :py:func:`iguazu.functions.galvanic.galvanic_clean`.
+        glitch_kwargs: see the documentation of :py:func:`galvanic_clean`.
+        interpolation_kwargs: see the documentation of :py:func:`galvanic_clean`.
+        lowpass_kwargs: see the documentation of :py:func:`galvanic_clean`.
+        scaling_kwargs: see the documentation of :py:func:`galvanic_clean`.
+        corrupted_maxratio: see the documentation of :py:ref:`galvanic_clean`.
+        force: if True, the task will run even if `output_group` from the output
+         HDF5 file already contains some data.
+        kwargs: additive keywords arguments to call the `run` method.
+        '''
         super().__init__(**kwargs)
         self.signal_group = signal_group
         self.events_group = events_group
@@ -132,7 +120,7 @@ class CleanSignal(prefect.Task):
         events_file = events.file.resolve()
 
         with pd.HDFStore(signal_file, 'r') as signal_store, \
-             pd.HDFStore(events_file, 'r') as events_store:
+                pd.HDFStore(events_file, 'r') as events_store:
 
             try:
                 # TODO discuss: select column before sending it to a column
@@ -180,52 +168,6 @@ class CleanSignal(prefect.Task):
         output._local_path = None
 
         return output
-
-
-@task
-def etl_galvanic_cvx(io_filenames,  tranform_params, force=False, input_paths = {"data": "/gsr/timeseries/preprocessed"},  output_path = "/gsr/timeseries/deconvoluted"):
-    """
-    This task is a basic ETL where the input and output are HDF5 files and where the transformation is made on a DataFrame.
-    It consists in loading the data from an input file, applying some processing (transformations) and
-    saving it into a file (same or other).
-
-    Parameters
-    ----------
-    io_filenames: dictionary with key 'input' and 'output', containing the input and output file names
-    tranform_params: dictionary with parameters to map when calling the transformation function
-    force: whether or not to force the task if the output path already exists in the output DataFrame
-    input_paths: list of paths that are expected to be found in the input DataFrame
-    output_path: path where the result is stored in the output DataFrame
-
-    Returns
-    -------
-    output filename with new path /gsr/timeseries/deconvoluted
-    """
-
-    input_filename = io_filenames["in"]
-    output_filename = io_filenames["out"]
-
-    if not force and path_exists_in_hdf5(output_filename, output_path):
-        return str(output_filename)
-
-    data_params = {key: safe_read_hdf5(input_filename, input_paths[key]) for key in input_paths}
-
-    print("Deconvolution from gsr in file {stem}.".format(stem=os.path.basename(input_filename)))
-
-    try:
-        data = galvanic_cvx(**data_params, **tranform_params)
-        meta = {"state": "success", "version": 0.0, "exception": None, "bad_ratio": data.bad.mean() }
-    except Exception as e:
-        data = pd.DataFrame()
-        meta = {"state": "fail", "version": 0.0, "exception": str(e), "bad_ratio": None}
-
-    # save in hdf
-    with pd.HDFStore(output_filename, "a") as store:
-        data.to_hdf(store, output_path)
-        store.get_node(output_path)._v_attrs['meta'] = meta
-    if data.empty:
-        raise signals.SKIP(message=str("cvx failed")) #TODO: I'm not sure how to handle failures
-    return str(output_filename)
 
 
 class ApplyCVX(prefect.Task):
@@ -314,54 +256,6 @@ class ApplyCVX(prefect.Task):
         return output
 
 
-@task
-def etl_galvanic_scrpeaks(io_filenames,  tranform_params, force=False,
-                          input_paths = {"data": "/gsr/timeseries/deconvoluted"}, output_path = "/gsr/timeseries/scrpeaks"):
-    """
-    This task is a basic ETL where the input and output are HDF5 files and where the transformation is made on a DataFrame.
-    It consists in loading the data from an input file, applying some processing (transformations) and
-    saving it into a file (same or other).
-
-    Parameters
-    ----------
-    io_filenames: dictionary with key 'input' and 'output', containing the input and output file names
-    tranform_params: dictionary with parameters to map when calling the transformation function
-    force: whether or not to force the task if the output path already exists in the output DataFrame
-    input_paths: list of paths that are expected to be found in the input DataFrame
-    output_path: path where the result is stored in the output DataFrame
-
-    Returns
-    -------
-    output filename with new path /gsr/timeseries/scrpeaks
-    """
-
-
-    input_filename = io_filenames["in"]
-    output_filename = io_filenames["out"]
-
-    if not force and path_exists_in_hdf5(output_filename, output_path):
-        return str(output_filename)
-
-    data_params = {key: safe_read_hdf5(input_filename, input_paths[key]) for key in input_paths}
-
-    print("Extraction of SCR Peaks characteristics from gsr in file {stem}.".format(stem=os.path.basename(input_filename)))
-
-    try:
-        data = galvanic_scrpeaks(**data_params, **tranform_params)
-        meta = {"state": "success", "version": 0.0, "exception": None, "bad_ratio": data.bad.mean() }
-    except Exception as e:
-        data = pd.DataFrame()
-        meta = {"state": "fail", "version": 0.0, "exception": str(e), "bad_ratio": None}
-
-    # save in hdf
-    with pd.HDFStore(output_filename, "a") as store:
-        data.to_hdf(store, output_path)
-        store.get_node(output_path)._v_attrs['meta'] = meta
-    if data.empty:
-        raise signals.SKIP(message="scrpeaks failed") #TODO: I'm not sure how to handle failures
-    return str(output_filename)
-
-
 class DetectSCRPeaks(prefect.Task):
 
     def __init__(self,
@@ -383,6 +277,28 @@ class DetectSCRPeaks(prefect.Task):
         self.force = force
 
     def run(self, signal: FileProxy) -> FileProxy:
+        '''
+        This task is a basic ETL where the input and output are HDF5 file proxy
+        and where the transformation is made on a DataFrame.
+        It consists in loading the signals and events from the input files proxy,
+        applying some processing (transformations) and
+        saving the result into an output file proxy.
+
+        The transformation that is performed is a detection of SCR peaks and the
+        estimation of their characteristics.
+        See the documentation of :func:`galvanic.galvanic_scrpeaks`.
+
+        Parameters
+        ----------
+        signal: file proxy with input signals.
+        events:  file proxy with input events.
+
+        Returns
+        -------
+        output: file proxy with transformed signals.
+
+        '''
+
         output = signal.make_child(suffix='_scr')
         self.logger.info('Galvanic SCR peak detection for %s -> %s', signal, output)
 
