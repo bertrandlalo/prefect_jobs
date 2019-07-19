51048be752826c18f7accce96bd70f87b272d507
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-07-19 17:34:47 +0200

    Helpers for tasks of type 'processing', until we create ZE base class

--- iguazu/helpers/tasks.py
+++ iguazu/helpers/tasks.py
@@ -1,7 +1,14 @@
+import gc
+
+import pandas as pd
 from prefect.engine.runner import ENDRUN
 
 from iguazu import __version__ as iguazu_version
-from iguazu.helpers.states import GracefulFail
+from iguazu.helpers.states import GracefulFail, GRACEFULFAIL, SKIPRESULT
+
+
+class IguazuError(Exception):
+    pass
 
 
 def get_base_meta(task, state=None, **kwargs):
@@ -16,9 +23,34 @@ def get_base_meta(task, state=None, **kwargs):
     return meta
 
 
-def graceful_fail(meta, output, state='FAILURE'):
-    if meta.get('state', None) == state:
-        # Until https://github.com/PrefectHQ/prefect/issues/1163 is fixed,
-        # this is the only way to skip with results
-        grace = GracefulFail('Task failed but generated empty dataframe', result=output)
-        raise ENDRUN(state=grace)
+def task_fail(task, ex, output, output_group, mode='grace'):
+    state = 'FAILURE'
+    if mode in ('grace', 'skip'):
+        # if fail_mode == 'grace': ==> generate empty dataframe, set metadata, return file (prefect raises success)
+        # if fail_mode == 'skip':  ==> generate empty dataframe, set metadata, raise skip
+        meta = get_base_meta(task, state=state, exception=str(ex))
+        # Update iguazu metadata with the current task
+        output.metadata['iguazu'].update({task.name: meta, 'state': state})
+        task_upload_result(task, pd.DataFrame(), meta, state, output, output_group)
+        if mode == 'grace':
+            grace = GracefulFail(ex, result=output)
+            raise GRACEFULFAIL(state=grace)
+        else:  # mode == 'skip':
+            raise SKIPRESULT(ex)
+    else:  # mode == 'fail'
+        # if fail_mode == 'fail':  ==> raise exception as it arrives
+        raise ENDRUN
+
+
+def task_upload_result(task, data, meta, state, output, output_group):
+    # Manage output, save to file
+    output_file = task.output.file
+    with pd.HDFStore(output_file, 'w') as output_store:
+        data.to_hdf(output_store, output_group)
+    # Update iguazu metadata with the current task
+    output.metadata['iguazu'].update({task.name: meta, 'state': state})
+    # Save memory, hdf5 is very bad at keeping memory
+    # TODO:  remove this in favor of a state_handler that calls the gc
+    task.logger.info('Calling gc...')
+    gc.collect()
+    task.output.upload()
