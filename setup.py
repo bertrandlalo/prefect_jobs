8ae180185c6f8e0b09d697727fcc6d4a321fae77
David Ojeda <david.ojeda@gmail.com>
2019-07-25 11:40:46 +0200

    Freeze versions of all dependencies

--- setup.py
+++ setup.py
@@ -2,26 +2,24 @@
 
 from setuptools import setup, find_packages
 
-# import versioneer
-
 
 dependencies = [
-    "APScheduler",
-    "numpy",
-    "scipy",
-    "pandas",
-    "matplotlib",
-    "tables",
-    "tzlocal",
-    "simplegeneric",
-    "click",
-    "prefect",
-    "graphviz",
-    "docker",
-    "coloredlogs",
-    'dsu @ git+https://github.com/OpenMindInnovation/datascience_utils@v0.3.0#egg=dsu',
-    "quetzal-client @ git+https://github.com/quetz-al/quetzal-client.git@268b5f9#egg=quetzal-client"
- ]
+    'APScheduler>=3.6,<4.0',
+    'bokeh>=1.3,<2.0',
+    'click>=7.0,<8.0',
+    'dsu @ git+https://github.com/OpenMindInnovation/datascience_utils@v0.3.0',
+    'numpy>=1.16,<2.0',
+    'pandas>=0.25.0,<0.26.0',
+    'prefect @ git+https://github.com/PrefectHQ/prefect@c7c00456',
+    'quetzal-client @ git+https://github.com/quetz-al/quetzal-client.git@v0.3.0',
+    'quetzal-openapi-client @ '
+    'git+https://github.com/quetz-al/quetzal-openapi-client@v0.3.0',
+    'scikit-learn>=0.21.2,<0.22.0',
+    'scipy>=1.3,<2.0',
+    'simplegeneric>=0.8.1,<0.9.0',
+    'tables>=3.5,<4.0',
+    'tzlocal>=2.0,<3.0'
+]
 build_dependencies = dependencies + ['pytest-runner']
 test_dependencies = ['pytest']
 authors = [
@@ -41,7 +39,7 @@ setup_args = dict(
     author=author_names,
     author_email=author_emails,
     install_requires=dependencies,
-    python_requires='>=3.6',
+    python_requires='>=3.7,<4.0',
     zip_safe=False,
     entry_points={
         'console_scripts': [
