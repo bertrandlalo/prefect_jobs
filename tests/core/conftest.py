7cdd5852f16c0723994bbc7c9d4b0fb800112697
David Ojeda <david.ojeda@gmail.com>
2020-03-09 19:19:52 +0100

    Document new fixture

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -6,6 +6,10 @@ from iguazu.core.files import LocalURL
 
 @pytest.fixture(scope='function')
 def temp_url(tmpdir):
+    """Create a temporary LocalURL file and set a prefect context
+
+    This avoids doing doing ``with prefect.context(temp_url=...):``
+    """
     url = LocalURL(path=tmpdir)
     with prefect.context(temp_url=url):
         yield url
