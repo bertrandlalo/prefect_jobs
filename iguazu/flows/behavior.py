e9c943528611100db356cd0eba664c360a297bc8
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-03-11 13:19:01 +0100

    Add behavior summarization flow

--- iguazu/flows/behavior.py
+++ iguazu/flows/behavior.py
@@ -4,7 +4,7 @@ from iguazu.core.flows import PreparedFlow
 from iguazu.flows.datasets import GenericDatasetFlow
 from iguazu.functions.behavior import SequenceNotFound
 from iguazu.tasks.behavior import SpaceStressFeatures
-from iguazu.tasks.common import SlackTask
+from iguazu.tasks.common import LoadDataframe, MergeDataframes, SlackTask
 from iguazu.tasks.metadata import CreateFlowMetadata, UpdateFlowMetadata
 from iguazu.tasks.standards import Report
 
@@ -80,6 +80,77 @@ class BehaviorFeaturesFlow(PreparedFlow):
             notify(message=message)
 
         logger.debug('Built flow %s with tasks %s', self, self.tasks)
+
+    @staticmethod
+    def click_options():
+        return GenericDatasetFlow.click_options()
+
+
+class BehaviorSummaryFlow(PreparedFlow):
+    """Collect all  cardiac features in a single CSV file"""
+
+    REGISTRY_NAME = 'summarize_behavior'
+    DEFAULT_QUERY = f"""
+        SELECT
+               base->>'id'       AS id,        -- id is the bare minimum needed for the query task to work
+               base->>'filename' AS filename  -- this is just to help the human debugging this
+        FROM   metadata
+        WHERE  base->>'state' = 'READY'                -- No temporary files
+        AND    base->>'filename' LIKE '%.hdf5'         -- Only HDF5 files TODO: remove _gsr_features hack
+        AND    iguazu->>'status' = 'SUCCESS'           -- Files that were successfully standardized
+        AND    standard->'features' ? '/iguazu/features/behavior' -- containing the GSR signal
+        ORDER BY id -- always in the same order
+    """
+
+    def _build(self,
+               **kwargs):
+
+        # Manage parameters
+        kwargs = kwargs.copy()
+        # Propagate workspace name because we captured it on kwargs
+        # kwargs['workspace_name'] = workspace_name
+        # Force required families: Quetzal workspace must have the following
+        # families: (nb: None means "latest" version)
+        required_families = dict(
+            iguazu=None,
+            omind=None,
+            standard=None,
+            protocol=None
+        )
+        families = kwargs.get('families', {}) or {}  # Could be None by default args
+        for name in required_families:
+            families.setdefault(name, required_families[name])
+        kwargs['families'] = families
+
+        # When the query is set by kwargs, leave the query and dialect as they
+        # come. Otherwise, set to the default defined just above
+        if not kwargs.get('query', None):
+            kwargs['query'] = self.DEFAULT_QUERY
+            kwargs['dialect'] = 'postgresql_json'
+
+        # Manage connections to other flows
+        dataset_flow = GenericDatasetFlow(**kwargs)
+        self.update(dataset_flow)
+        features_files = dataset_flow.terminal_tasks().pop()
+
+        # instantiate tasks. Use separate tasks for a classic ETL approach:
+        # E: read features from HDF5 file
+        # T and L: merge features into a single dataframe, then save as CSV
+        read_features = LoadDataframe(
+            key='/iguazu/features/behavior',
+        )
+        merge_features = MergeDataframes(
+            filename='behavior_summary.csv',
+            path='datasets',
+        )
+
+        notify = SlackTask(message='Behavioral features summarization finished!')
+
+        with self:
+            feature_dataframes = read_features.map(file=features_files)
+            merged_dataframe = merge_features(parents=features_files, dataframes=feature_dataframes)
+            # Send slack notification
+            notify(upstream_tasks=[merged_dataframe])
 
     @staticmethod
     def click_options():
