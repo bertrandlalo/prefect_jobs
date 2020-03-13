0342635e9849ea03d120848d475cdc298ae84e90
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-03-13 13:55:39 +0100

    Fix meanE

--- iguazu/functions/respiration.py
+++ iguazu/functions/respiration.py
@@ -1,15 +1,18 @@
 import logging
-from dataclasses import dataclass, field
 
-import neurokit2 as nk  # todo: add this dependency
 import numpy as np
 import pandas as pd
-from dsu.pandas_helpers import estimate_rate
-
-from iguazu.core.features import dataclass_to_dataframe
-from iguazu.functions.unity import VALID_SEQUENCE_KEYS
 
 logger = logging.getLogger(__name__)
+
+from dsu.pandas_helpers import estimate_rate
+import neurokit2 as nk  # todo: add this dependency
+
+from dataclasses import dataclass, field
+from iguazu.core.features import dataclass_to_dataframe
+
+from iguazu.functions.unity import VALID_SEQUENCE_KEYS
+
 
 class NoRespirationPeaks(Exception):
     pass
@@ -134,7 +137,7 @@ def respiration_features(data):
     if not i_duration.empty:
         features.meanI = i_duration.mean()
     # expiration duration
-    e_duration = data.E_duration.dropna()
+    e_duration = data.I_duration.dropna()
     if not e_duration.empty:
         features.meanE = e_duration.mean()
     ie_duration = data.IE_duration.dropna()
